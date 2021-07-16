4071ea8310e9a7f751c138bf5c98995c9251ee6e
Jonathan <jonathan.prieto@me.com>
2021-07-16 09:33:33 -0500

    First Upload

---
+++ cmake-build-debug/CMakeFiles/pc2_grupon.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pc2_grupon.dir/main.c.o"
+  "pc2_grupon"
+  "pc2_grupon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pc2_grupon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pc2_grupon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pc2_grupon.
+# This may be replaced when dependencies are built.
