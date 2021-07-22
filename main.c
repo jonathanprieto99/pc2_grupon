035692934a9df9f6ca176fb852cc946b055a7f92
Jonathan <jonathan.prieto@me.com>
2021-07-21 19:59:47 -0500

    Se agregaron el parser.

--- main.c
+++ main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void request(char process[2], int size, char typefit[1]){
+    printf("Se hace un request para el proceso: %s de tamaño: %d con un fit de tipo: %s", process, size, typefit);
+}
+
+void release(char process[2]){
+    printf("Se hace el release del proceso: %s",process);
+}
+
+void compact(){
+    printf("Entro a Compact");
+}
+
+void status_report(){
+    printf("Entro a Reporte de Status");
+}
+
+void allocate_memory(int size){
+    printf("The size of the memory will be: %d\n", size);
+}
 
 int main() {
-    printf("Hello, World!\n");
+
+    char str[100];
+    gets( str );
+    int size = atoi(str);
+
+    allocate_memory(size);
+    char process[2];
+    char command[4];
+    int bytes;
+    char type_fit;
+
+    while(1){
+        printf("allocator>");
+        scanf("%s %s %d %s" , &command, &process, &bytes, &type_fit);
+        printf(command);
+        if (strcmp(command, "STAT") == 0){
+            status_report();
+        } else if(strcmp(command,"RQ") == 0){
+            request(process, bytes, type_fit);
+        } else if(strcmp(command,"RL") == 0){
+            release(process);
+        } else if(strcmp(command, "C") == 0){
+            compact();
+        } else if(strcmp(command, "X") == 0){
+            break;
+        }
+        continue;
+    }
+
+
+
     return 0;
 }
